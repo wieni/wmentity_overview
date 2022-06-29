d7a618735f12d1d92bde2d8d3a3331979c1763b1
Dieter Holvoet <dieter.holvoet@gmail.com>
2022-06-29 17:07:13 +0200

    Replace usage of Symfony\Component\EventDispatcher\Event & dump core version

--- src/Event/EntityOverviewAlterEvent.php
+++ src/Event/EntityOverviewAlterEvent.php
@@ -2,9 +2,9 @@
 
 namespace Drupal\wmentity_overview\Event;
 
+use Drupal\Component\EventDispatcher\Event;
 use Drupal\wmentity_overview\Annotation\OverviewBuilder;
 use Drupal\wmentity_overview\OverviewBuilder\OverviewBuilderInterface;
-use Symfony\Component\EventDispatcher\Event;
 
 class EntityOverviewAlterEvent extends Event
 {
